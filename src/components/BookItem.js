03a2ce14406901d22041a0a83c9e61f7f5cc685d
AhmedEssam74 <ae4418347@gmail.com>
2023-07-24 22:32:07 +0300

    'test'

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 const BookItem = ({ book }) => {
-    // const [test , setTest] = useState() 
     
     return (
         <Card>
